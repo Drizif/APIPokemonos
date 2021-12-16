54dd44d225ba4cef2813b4b4b895d60047fbf5cf
Drizif <joseluisrdzs@live.com>
2021-12-15 18:56:33 -0600

    fix token verify

--- routes/trainer.routes.js
+++ routes/trainer.routes.js
@@ -4,7 +4,7 @@ const Middleware = require('../middlewares/trainer.middlewares');
 const Token = require('../middlewares/token.middlewares');
 
 router.post('/trainer', Middleware.createTrainer, Controller.createTrainer);
-router.post('/login', Token.verify, Middleware.login, Controller.login);
+router.post('/login', Middleware.login, Controller.login);
 router.get('/trainer', Token.verify, Middleware.getTrainer, Controller.getTrainer);
 router.put('/trainer', Token.verify, Middleware.updateTrainer, Controller.updateTrainer);
 router.delete('/trainer', Token.verify, Middleware.deleteTrainer, Controller.deleteTrainer);
