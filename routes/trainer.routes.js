25f76e13d27b4ed57bec9dac7f08e92ca363683a
Drizif <joseluisrdzs@live.com>
2021-12-16 19:32:00 -0600

    no token

--- routes/trainer.routes.js
+++ routes/trainer.routes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 const Controller = require('../controllers/trainer.controllers');
 const Middleware = require('../middlewares/trainer.middlewares');
-const Token = require('../middlewares/token.middlewares');
 
 router.post('/trainer', Middleware.createTrainer, Controller.createTrainer);
 router.post('/login', Middleware.login, Controller.login);
-router.get('/trainer', Token.verify, Middleware.getTrainer, Controller.getTrainer);
-router.put('/trainer', Token.verify, Middleware.updateTrainer, Controller.updateTrainer);
-router.delete('/trainer', Token.verify, Middleware.deleteTrainer, Controller.deleteTrainer);
+router.get('/trainer', Middleware.getTrainer, Controller.getTrainer);
+router.put('/trainer', Middleware.updateTrainer, Controller.updateTrainer);
+router.delete('/trainer', Middleware.deleteTrainer, Controller.deleteTrainer);
 
 module.exports = router;
